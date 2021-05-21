9986b60e0bc5c0b16c14e4b6c4bd4d782fa5b084
DiegoCol93 <dfelo93@gmail.com>
2021-05-21 18:56:51 -0500

    |- ADDED -| Loading bar.

--- modules/show_result.py
+++ modules/show_result.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """ Module for storing the show_result method. """
+from os import get_terminal_size
 from sys import argv as av
 from requests import get
 from os import path
-from tqdm import tqdm
+from time import sleep
 
 
-def show_result(correction_id='5028906'):
+def show_result(correction_id='5029461'):
     """ Prints the result of the correction """
+
+    size = get_terminal_size()
+    col = size.columns
 
     if path.exists('/tmp/.hbnb_auth_token') is None:
         print("No /tmp/.hbnb_auth_token file...")
@@ -17,13 +21,23 @@ def show_result(correction_id='5028906'):
         auth = f.read()
 
     url = 'https://intranet.hbtn.io/correction_requests/{}.json?auth_token={}' \
-        .format(correction_id, auth)
+    .format(correction_id, auth)
 
     response = get(url)
 
-    for i in tqdm(range(10000)):
-        pass
-    print(response.json())
+    while response.json()['status'] != 'Done':
+        sleep(0.5)
+        i = 1
+        for i in range(col):
+            sleep(0.2)
+            print('|' * i)
+            # print("Checking your code... {}".format(char))
+            print("\033[1;0f")
+            i += 1
+        response = get(url)
 
-
-show_result()
+    for check in response.json()['result_display']['checks']:
+        check_type = check['check_label']
+        appproved = check['passed']
+        title = check['title']
+        print("{}: Approved: {} Type: {}".format(check['title'],check['passed'],check['check_label']))
